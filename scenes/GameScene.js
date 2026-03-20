f4de75aa3561838ca301769e04f26dcf52d84bbb
Nebulocity <Nebulocity@users.noreply.github.com>
2026-03-20 08:54:13 -0700

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -446,7 +446,7 @@ export default class GameScene extends Phaser.Scene {
     const cx = zone.x + zone.w / 2;
     const cy = zone.y + zone.h - 80;
 
-    const sprite = this.add.sprite(cx, cy + 140, 'shaman_idle', 0)
+    const sprite = this.add.sprite(cx, cy, 'shaman_idle', 0)
       .setScale(1)
       .setOrigin(0.5, 1);
 
